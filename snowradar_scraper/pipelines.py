eb207d424f64fedcd212c2d161c1558d68a89a6d
AurelDeveloper <marlon_aurel@yahoo.com>
2025-01-03 23:13:36 +0100

    EDIT: clean the pipeline code

--- snowradar_scraper/pipelines.py
+++ snowradar_scraper/pipelines.py
@@ -6,40 +6,31 @@ class OnthesnowCleaningPipeline:
         adapter = ItemAdapter(item)
         required_fields = ['name', 'snowfall_24h', 'base_depth']
         
-        # Überprüfen, ob die erforderlichen Felder vorhanden sind
+        # Check if required fields are present
         if not all(adapter.get(field) for field in required_fields):
             raise DropItem(f"Missing required fields in {item}")
         
-        # Bereinigen des 'name'-Feldes
+        # Clean 'name' field
         adapter['name'] = adapter['name'].strip()
         
-        # Bereinigen der numerischen Felder
+        # Clean numeric fields
         numeric_fields = ['snowfall_24h', 'base_depth', 'open_trails', 'total_trails', 'open_lifts', 'total_lifts']
         for field in numeric_fields:
-            if field == 'base_depth':
-                adapter[field] = self.clean_base_depth(adapter.get(field))
-            else:
-                adapter[field] = self.clean_numeric(adapter.get(field))
+            adapter[field] = self.clean_base_depth(adapter.get(field)) if field == 'base_depth' else self.clean_numeric(adapter.get(field))
         
         return item
 
     def clean_numeric(self, value):
-        # Wenn der Wert None ist, auf 0.0 setzen
         if value is None:
             return 0.0
-        # Entfernen von Anführungszeichen und Leerzeichen
         value = value.replace('"', '').strip()
         try:
-            # Umwandeln in eine Gleitkommazahl
             return float(value)
         except ValueError:
-            # Wenn ein Fehler auftritt, auf 0.0 setzen
             return 0.0
 
     def clean_base_depth(self, value):
         if '-' in value:
             min_val, max_val = value.split('-')
-            min_val = self.clean_numeric(min_val)
-            max_val = self.clean_numeric(max_val)
-            return (min_val + max_val) / 2
+            return (self.clean_numeric(min_val) + self.clean_numeric(max_val)) / 2
         return self.clean_numeric(value)
